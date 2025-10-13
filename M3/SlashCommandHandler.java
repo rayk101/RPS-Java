06f794c786f3144595cd4c8a93fe4c05c46793a7
rutgershealthhackathon67-ops <rutgershealthhackathon67@gmail.com>
2025-10-13 17:18:23 -0400

    Added steps to solve

--- M3/SlashCommandHandler.java
+++ M3/SlashCommandHandler.java
@@ -13,6 +13,11 @@ Challenge 2: Simple Slash Command Handler
 - Print errors for invalid command formats (when applicable)
 - Capture 3 variations of each command except "/quit"
 - Date: 10/13/25
+- Steps to Solve: 1. Capture the input from the console using scanner.line  
+2. conver the the input to lowercase using toLowerCase() and then get rid of whitespace 
+3. find out which slash command was used and extract the arguments,using startswith() or equalIgnoreCase() 
+4. perform the action - correct response. Using a while loop to go over responses 
+5. handle any errors usign a while a clause. 
 */
 
 import java.util.Scanner;
