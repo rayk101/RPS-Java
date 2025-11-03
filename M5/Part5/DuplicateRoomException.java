2feabed6301901800330405f8d03b2d41232d22d
rutgershealthhackathon67-ops <rutgershealthhackathon67@gmail.com>
2025-11-03 16:19:37 -0500

    all m5/m4 files

---
+++ M5/Part5/DuplicateRoomException.java
@@ -0,0 +1,13 @@
+
+package M5.Part5;
+
+public class DuplicateRoomException extends CustomIT114Exception {
+    public DuplicateRoomException(String message) {
+        super(message);
+    }
+
+    public DuplicateRoomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ M5/Part5/RoomNotFoundException.java
@@ -0,0 +1,14 @@
+
+package M5.Part5;
+
+public class RoomNotFoundException extends CustomIT114Exception {
+
+    public RoomNotFoundException(String message) {
+        super(message);
+    }
+
+    public RoomNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
